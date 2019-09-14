e9d0a4670d04a35642bd5ff980dcd759927e92f1
YujieLyu <lyuy4@student.unimelb.edu.au>
2019-09-14 10:48:30 +1000

    Replace related links to the official version

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 include_once('header.php');
 ?>
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col" style="background-color:lavender;">
+
+        </div>
+        <div class="col" style="background-color:orange;">
+
+        </div>
+        <div class="col" style="background-color:lavender;">
+
+        </div>
+    </div>
+</div>
