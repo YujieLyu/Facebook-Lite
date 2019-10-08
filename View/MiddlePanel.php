25a0445d94955607279bd5e28e44fe7c19a501e9
YujieLyu <lyuy4@student.unimelb.edu.au>
2019-10-09 00:14:48 +1100

    Reply, Like & Comment UI ongoing

--- View/MiddlePanel.php
+++ View/MiddlePanel.php
@@ -104,43 +104,31 @@ endif;
                 }
             }
         </script>
-        <!--Likes & comments buttons-->
-        <div class="input-group mt-n3">
+    </div>
+    <!--Likes & comments buttons-->
+    <div class="row mt-2">
+        <div class="col-6 text-center">
             <form action="../router.php" method="post">
                 <input type="hidden" name="postID" value="<?php echo $post_id ?>">
                 <input type="hidden" name="likerID" value="<?php echo $userID ?>">
                 <input type="submit" name="Like" class="btn btn-light w-50  text-dark" value="Like">
-                <input type="button" class="btn btn-light w-50 text-dark" value="Comment"
-                       onclick="showReplyBox<?php echo $post_id ?>()">
+
             </form>
-
+        </div>
+        <div class="col-6 text-center text-dark ">
+            <input type="button" class="btn btn-light w-50 text-dark" value="Comment"
+                   onclick="showReplyBox<?php echo $post_id ?>()">
         </div>
 
+        <hr/>
 
-        <div id="replyBox<?php echo $post_id ?>" class="m-4" style="display: none;">
-            <div class="row">
-                <div class="col-1">
-                    <img class="rounded-circle mx-2 my-2" src="../resources/Yujie.JPG" alt="avatar"
-                         style="width: 40px;">
-                </div>
-                <div class="col-10">
-                    <form action="../router.php" method="post">
-                        <input type="hidden" name="postID" value="<?php echo $post_id ?>">
-                        <input type="hidden" name="replierID" value="<?php echo $userID ?>">
-                        <textarea name="replyContent" id="reply" class="form-control text-dark mt-2 mx-4" rows="1"
-                                  style="resize: none"></textarea>
-                        <input type="submit" name="Comment"
-                               class="btn text-white mt-2 border-0 float-sm-right px-2"
-                               style="background: #3b5998"
-                               value="Reply">
-                    </form>
-                </div>
-            </div>
-
-        </div>
         <script>
-            function showReplyBox<?php echo $post_id ?>() {
-                var x = document.getElementById("replyBox<?php echo $post_id ?>");
+            $(document).ready(function () {
+                $('[data-toggle="tooltip"]').tooltip();
+            });
+
+            function showReplyList<?php echo $comment_no ?>() {
+                let x = document.getElementById("replyList<?php echo $comment_no ?>");
                 if (x.style.display === "none") {
                     x.style.display = "block";
                 } else {
@@ -148,21 +136,66 @@ endif;
                 }
             }
         </script>
-        <div id="replyList<?php echo $comment_no ?>" class="m-4" style="display: none;">
-            <div class="row">
-                <div class="col-1">
-                    <img class="rounded-circle mx-2 my-2" src="../resources/Yujie.JPG" alt="avatar"
-                         style="width: 40px;">
-                </div>
-                <div class="col-10">
-                    <p>
-                        <?php echo $comment_content ?>
-                    </p>
-                </div>
+    </div>
+<!--    <div class="input-group mt-n3">-->
+<!--        <form action="../router.php" method="post">-->
+<!--            <input type="hidden" name="postID" value="--><?php //echo $post_id ?><!--">-->
+<!--            <input type="hidden" name="likerID" value="--><?php //echo $userID ?><!--">-->
+<!--            <input type="submit" name="Like" class="btn btn-light w-50  text-dark" value="Like">-->
+<!--            <input type="button" class="btn btn-light w-50 text-dark" value="Comment"-->
+<!--                   onclick="showReplyBox--><?php //echo $post_id ?>
+<!--//        </form>-->
+<!--//-->
+<!--//    </div>-->
+
+
+    <div id="replyBox<?php echo $post_id ?>" class="m-4" style="display: none;">
+        <div class="row">
+            <div class="col-1">
+                <img class="rounded-circle mx-2 my-2" src="../resources/Yujie.JPG" alt="avatar"
+                     style="width: 40px;">
+            </div>
+            <div class="col-10">
+                <form action="../router.php" method="post">
+                    <input type="hidden" name="postID" value="<?php echo $post_id ?>">
+                    <input type="hidden" name="replierID" value="<?php echo $userID ?>">
+                    <textarea name="replyContent" id="reply" class="form-control text-dark mt-2 mx-4" rows="1"
+                              style="resize: none"></textarea>
+                    <input type="submit" name="Comment"
+                           class="btn text-white mt-2 border-0 float-sm-right px-2"
+                           style="background: #3b5998"
+                           value="Reply">
+                </form>
+            </div>
+        </div>
+
+    </div>
+    <script>
+        function showReplyBox<?php echo $post_id ?>() {
+            var x = document.getElementById("replyBox<?php echo $post_id ?>");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+    </script>
+    <div id="replyList<?php echo $comment_no ?>" class="m-4" style="display: none;">
+        <div class="row">
+            <div class="col-1">
+                <img class="rounded-circle mx-2 my-2" src="../resources/Yujie.JPG" alt="avatar"
+                     style="width: 40px;">
+            </div>
+            <div class="col-10">
+                <p>
+                    <?php echo $comment_content ?>
+                </p>
             </div>
         </div>
     </div>
+</div>
     <?php
     endforeach;
     endif;
     ?>
+
